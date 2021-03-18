90008b385da9e0d9186fc57f97122b06902a2225
Venky <Venky@DESKTOP-EPM05SP>
2021-03-18 07:20:36 +0530

    Done some changes

--- MyProject/src/test/java/myPackage/Assignment.java
+++ MyProject/src/test/java/myPackage/Assignment.java
@@ -12,7 +12,7 @@ import junit.framework.Assert;
 public class Assignment {
 	
 	@Test
-	public void test()
+	public void PlacingOrder()
 	 {
 	try 
 	{
@@ -22,30 +22,17 @@ public class Assignment {
 	// 1. Navigate to URL : http://demowebshop.tricentis.com/
 	driver.get("http://demowebshop.tricentis.com");
 	driver.manage().window().maximize();
-	//--------------------
 	
 	// 2. Click on Login button
 	WebElement loginLink = driver.findElement(By.linkText("Log in"));
 	loginLink.click();
-	//--------------------
-	
-	////h1[contains(text(),'Welcome, Please Sign In!')]
+		
 	// 3. Validate “Welcome, Please Sign In!” message
 	WebElement SignInMsg = driver.findElement(By.xpath("//h1[contains(text(),'Welcome, Please Sign In!')]"));
-	
 	String actualMsg = SignInMsg.getText();
 	Assert.assertEquals(actualMsg, "Welcome, Please Sign In!");
-	/*if (actualMsg.equals("Welcome, Please Sign In!"))
-	{
-		System.out.println("Welcome message is matching ");
-	}
-	else {
-		System.out.println("Welcome message is not matching ");
-	}*/
-	//--------------------
-	
+		
 	//4. Log in with given credentials
-	
 	WebElement email = driver.findElement(By.xpath("//input[@id='Email']"));
 	email.sendKeys("atest@gmail.com");
 	WebElement password = driver.findElement(By.xpath("//input[@id='Password']"));
@@ -53,20 +40,10 @@ public class Assignment {
 	WebElement loginButton = driver.findElement(By.xpath("//body/div[4]/div[1]/div[4]/div[2]/div[1]/div[2]/div[1]/div[2]/div[2]/form[1]/div[5]/input[1]"));
 	loginButton.click();
 	
-	//--------------------
-	
 	// 5. Validate the user account ID on top right
 	WebElement account = driver.findElement(By.xpath("//a[contains(text(),'atest@gmail.com')]"));
 	String AccountID = account.getText();
 	Assert.assertEquals(AccountID, "atest@gmail.com");
-	/*if (AccountID.equals("atest@gmail.com"))
-	{
-		System.out.println("account ID is matching ");
-	}
-	else {
-		System.out.println("account ID is not matching ");
-	}*/
-	//--------------------
 	
 	//6. Clear the shopping cart
 	try
@@ -77,27 +54,22 @@ public class Assignment {
 	WebElement CartStatus = driver.findElement(By.xpath("//div[contains(text(),'Your Shopping Cart is empty!')]"));
 	String CartStatus1 = CartStatus.getText();
 	Assert.assertEquals(CartStatus1, "Your Shopping Cart is empty!");
-	/*if (CartStatus1.equals("Your Shopping Cart is empty!"))
-	{
-		System.out.println("Shopping cart is cleared");
-	}
-	else {
-		System.out.println("Shopping cart is not cleared");
-	}*/
 	}
 	catch(Exception e)
 	{
 		
 	}
-	//--------------------
+	
 	// 7. Select “Books” from Categories
 	driver.findElement(By.linkText("BOOKS")).click();
 	
-	//------------------
+	
 	// 8. Select a book from the list displayed
 	driver.findElement(By.linkText("Computing and Internet")).click();
 	
 	//9. Get the price details and enter the quantity (more than one)
+	String price = driver.findElement(By.xpath("//label[contains(text(),'Price:')]/following::span[1]")).getText();
+	System.out.println("Price value is : "+price);
 	WebElement Qty = driver.findElement(By.cssSelector("#addtocart_13_EnteredQuantity"));
 	Qty.clear();
 	Qty.sendKeys("2");
@@ -105,23 +77,17 @@ public class Assignment {
 	// 10. Click on “Add to cart”
 	driver.findElement(By.xpath("//input[@id='add-to-cart-button-13']")).click();
 	Thread.sleep(2000);
+	
 	//11. Validate “The product has been added to shopping cart” message
 	String productadded = driver.findElement(By.xpath("//body/div[@id='bar-notification']/p[1]")).getText();
 	Assert.assertEquals(productadded, "The product has been added to your shopping cart");
-	/*if (productadded.equals("The product has been added to your shopping cart"))
-	{
-		System.out.println("Product is added");
-	}
-	else {
-		System.out.println("Product is not added");
-	}*/
 	driver.findElement(By.xpath("//body/div[@id='bar-notification']/span[1]")).click();
 	Thread.sleep(2000);
+	
 	// 12. Click on “shopping cart” on top right and validate the “Sub-Total” Price for selected book.
 	driver.findElement(By.xpath("//span[contains(text(),'Shopping cart')]")).click();
 	
 	//13. Click on “Check-out”
-	
 	driver.findElement(By.xpath("//input[@id='termsofservice']")).click();
 	driver.findElement(By.xpath("//button[@id='checkout']")).click();
 	
@@ -129,8 +95,7 @@ public class Assignment {
 	WebElement billingAddress = driver.findElement(By.xpath("//select[@id='billing-address-select']"));
 	Select BillingAddress = new Select(billingAddress);
 	Thread.sleep(4000);
-	BillingAddress.selectByVisibleText("New Address");
-	
+	BillingAddress.selectByVisibleText("New Address");	
 	Thread.sleep(1000);
 	WebElement firstName = driver.findElement(By.xpath("//input[@id='BillingNewAddress_FirstName']"));
 	firstName.clear();
@@ -155,41 +120,46 @@ public class Assignment {
 	Thread.sleep(2000);
 	driver.findElement(By.xpath("//body/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/ol[1]/li[1]/div[2]/div[1]/input[1]")).click();
 	Thread.sleep(2000);
+	
 	//16. Select the “Shipping Address” as same as “Billing Address” from “Shipping Address” drop down and click on “Continue”
 	WebElement shippingAddress = driver.findElement(By.xpath("//select[@id='shipping-address-select']"));
 	Select ShippingAddress = new Select(shippingAddress);
-	Thread.sleep(3000);
+	Thread.sleep(4000);
 	ShippingAddress.selectByIndex(6);
 	Thread.sleep(2000);
 	driver.findElement(By.xpath("//body/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/ol[1]/li[2]/div[2]/div[1]/input[1]")).click();
 	Thread.sleep(1000);
+	
 	//17. Select the shipping method as “Next Day Air” and click on “Continue”
 	driver.findElement(By.xpath("//input[@id='shippingoption_1']")).click();
 	Thread.sleep(2000);
 	driver.findElement(By.xpath("//body/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/ol[1]/li[3]/div[2]/form[1]/div[2]/input[1]")).click();
 	Thread.sleep(1000);
+	
 	//18. Choose the payment method as COD (Cash on delivery) and click on “Continue”
 	driver.findElement(By.xpath("//input[@id='paymentmethod_0']")).click();
 	Thread.sleep(1000);
 	driver.findElement(By.xpath("//body/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/ol[1]/li[4]/div[2]/div[1]/input[1]")).click();
 	Thread.sleep(1000);
+	
 	//19. Validate the message “You will pay by COD” and click on “Continue”
 	String paymentInfo = driver.findElement(By.xpath("//tbody//tr//td//p")).getText();
 	Assert.assertEquals(paymentInfo, "You will pay by COD");
 	Thread.sleep(1000);
 	driver.findElement(By.xpath("//body/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/ol[1]/li[5]/div[2]/div[1]/input[1]")).click();
+	Thread.sleep(2000);
 	
 	//20. Click on “Confirm Order”
-	Thread.sleep(2000);
 	driver.findElement(By.xpath("//body/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/ol[1]/li[6]/div[2]/div[2]/input[1]")).click();
 	
 	//21. Validate the message “Your order has been successfully processed!” and print the Order number
 	Thread.sleep(1000);
 	String confirmMsg = driver.findElement(By.xpath("//strong[contains(text(),'Your order has been successfully processed!')]")).getText();
 	Assert.assertEquals(confirmMsg, "Your order has been successfully processed!");
-	//String OrderNo = driver.findElement(By.xpath("//li[contains(text(),'Order number: 888175')]")).getText();
-	//System.out.println("Order Number is: "+OrderNo);
+	String OrderNo = driver.findElement(By.xpath("//strong[contains(text(),'Your order has been successfully processed!')]/following::ul[1]/li[1]")).getText();
+	System.out.println("Order Number is: "+OrderNo);
 	Thread.sleep(1000);
+	
 	//22. Click on “Continue” and log out from the application 
 	driver.findElement(By.xpath("//body/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[2]/input[1]")).click();
 	Thread.sleep(1000);
@@ -199,23 +169,6 @@ public class Assignment {
 	catch(Exception e) {
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	}
 }
